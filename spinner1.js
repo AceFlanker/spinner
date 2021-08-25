246442f7c6e2e221229dcb41c0d3b3d12e8c81cf
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-24 22:28:46 -0700

    Initial commit

---
+++ spinner1.js
@@ -0,0 +1,32 @@
+const spinorama1 = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r|   ', '\r|   ', '\r|   ', '\r|   ', '\r\\   ', '\r-   ', '\r/   ', '\r|   ', '\r\\   ', '\r-   ', '\r/   ', '\r|   ', '\r\\   ', '\r-   ', '\r/   ', '\r|   '];
+const spinorama2 = ['\r|   ', '\r /   ', '\r   -   ', '\r    \\   ', '\r     |   ', '\r      /   ', '\r       -   ', '\r        \\   ', '\r         |   ', '\r          /   ', '\r           -   ', '\r            \\   ', '\r             |   ', '\r             |   ', '\r             |   ', '\r             |   ', '\r             |   ', '\r            \\   ', '\r           -   ', '\r          /   ', '\r         |   ', '\r        \\   ', '\r       -   ', '\r      /   ', '\r     |   ', '\r    \\   ', '\r   -   ', '\r  /   ', '\r |   ', '\r|   '];
+let counter = 0;
+
+// Attempt 1 
+const hopeThisWorks1 = function(stringRay) {
+  if (counter < stringRay.length) {
+    process.stdout.write(stringRay[counter]);
+    counter++;
+    setTimeout(() => {
+      hopeThisWorks1(stringRay);
+    }, 50);
+  } else {
+    process.stdout.write('\n');
+  }
+};
+
+hopeThisWorks1(spinorama1);
+
+// Attempt 2
+const hopeThisWorks2 = function(stringRay) {
+  for (let i = 0; i < stringRay.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(stringRay[i]);
+    }, i * 50);
+  }
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, stringRay.length * 50);
+};
+
+setTimeout(() => hopeThisWorks2(spinorama2), 1800);
